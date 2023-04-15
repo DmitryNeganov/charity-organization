4958ab69a16c7afe65daea33416d5c762f33f54f
Dmitry Neganov <glorynewfound@yandex.ru>
2023-04-15 14:55:02 +0300

    feature/controllers
    *project controller
    *donation controller

--- src/main/java/org/dvn/charity/organization/api/controller/ProjectController.java
+++ src/main/java/org/dvn/charity/organization/api/controller/ProjectController.java
@@ -1,9 +1,23 @@
 package org.dvn.charity.organization.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.dvn.charity.organization.persistence.entity.Project;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/projects")
 public class ProjectController {
+
+    @GetMapping("/")
+    public List<Project> getAllProjects() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Project getProjectById(@PathVariable("id") long id) {
+        return null;
+    }
+
+    @PostMapping
 }
---
+++ src/main/java/org/dvn/charity/organization/persistence/entity/DonationHistory.java
@@ -0,0 +1,13 @@
+package org.dvn.charity.organization.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "donations")
+@Getter
+@Setter
+public class DonationHistory {
+}
