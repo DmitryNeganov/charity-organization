a81e83b33a6c2aaa501f0958224aa0ea38a3c2cf
Dmitry Neganov <glorynewfound@yandex.ru>
2023-04-15 14:55:02 +0300

    feature/controllers
    *project controller
    *donation controller

--- src/main/java/org/dvn/charity/organization/api/controller/DonationController.java
+++ src/main/java/org/dvn/charity/organization/api/controller/DonationController.java
@@ -1,4 +1,36 @@
 package org.dvn.charity.organization.api.controller;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import org.dvn.charity.organization.persistence.entity.Donation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/donations")
 public class DonationController {
+
+
+    @GetMapping("/")
+    public List<Donation> findAllDonations() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Donation getDonationById(@PathVariable("id") long id) {
+        return null;
+    }
+
+    @PostMapping("/")
+    @Operation
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created"),
+            @ApiResponse(code = 400, message = "Missing or invalid request body"),
+            @ApiResponse(code = 500, message = "Internal error")})
+    public Donation createDonation() {
+        return null;
+    }
+
 }
