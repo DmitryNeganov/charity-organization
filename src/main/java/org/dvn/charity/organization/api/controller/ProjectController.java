31015927689339b786364ad017befd86294aa349
Dmitry Neganov <glorynewfound@yandex.ru>
2023-04-17 22:20:58 +0300

    feature

--- src/main/java/org/dvn/charity/organization/api/controller/ProjectController.java
+++ src/main/java/org/dvn/charity/organization/api/controller/ProjectController.java
@@ -1,19 +1,26 @@
 package org.dvn.charity.organization.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.dvn.charity.organization.api.dto.ProjectDto;
 import org.dvn.charity.organization.persistence.entity.Project;
-import org.dvn.charity.organization.persistence.repository.ProjectRepo;
 import org.dvn.charity.organization.service.ProjectService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
- * The type Project controller.
+ * Project controller.
+ * ProjectDto is used for the validation and post data.
  */
 @RestController
 @RequestMapping(value = "/projects")
@@ -25,13 +32,14 @@ public class ProjectController {
     /**
      * Gets all projects.
      *
-     * @return the all projects
+     * @return all projects
      */
     @GetMapping("/")
+    @Operation(summary = "Get all the projects")
     @ResponseStatus(HttpStatus.OK)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "404", description = "Schema not found"),
+            @ApiResponse(responseCode = "404", description = "Project not found"),
             @ApiResponse(responseCode = "500", description = "Internal error")})
     public List<Project> getAllProjects() {
         return projectService.getAllProjects();
@@ -44,58 +52,62 @@ public class ProjectController {
      * @return the project by id
      */
     @GetMapping("/{id}")
+    @Operation(summary = "Get project by ID")
     @ResponseStatus(HttpStatus.OK)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "404", description = "Schema not found"),
+            @ApiResponse(responseCode = "404", description = "Project not found"),
             @ApiResponse(responseCode = "500", description = "Internal error")})
     public Project getProjectById(@PathVariable("id") long id) {
         return projectService.getProjectById(id);
     }
 
     /**
-     * Create project project.
+     * Create project.
      *
-     * @param projectDto the project dto
+     * @param projectDto
      * @return the project
      */
     @PostMapping("/")
+    @Operation(summary = "Create project")
     @ResponseStatus(HttpStatus.CREATED)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Project created"),
-            @ApiResponse(responseCode = "404", description = "Schema not found"),
+            @ApiResponse(responseCode = "400", description = "Missing or invalid request body"),
             @ApiResponse(responseCode = "500", description = "Internal error")})
     public Project createProject(@RequestBody ProjectDto projectDto) {
         return projectService.createProject(projectDto);
     }
 
     /**
-     * Deactivate project project.
+     * Deactivate project.
      *
      * @param id the id
      * @return the project
      */
     @GetMapping("/{id}/deactivate")
+    @Operation(summary = "Deactivate the project by id")
     @ResponseStatus(HttpStatus.OK)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "404", description = "Schema not found"),
+            @ApiResponse(responseCode = "404", description = "Project not found"),
             @ApiResponse(responseCode = "500", description = "Internal error")})
     public Project deactivateProject(@PathVariable long id) {
         return projectService.deacrivateProject(id);
     }
 
     /**
-     * Activate project project.
+     * Activate project.
      *
      * @param id the id
      * @return the project
      */
     @GetMapping("/{id}/activate")
+    @Operation(summary = "Activate the project by id")
     @ResponseStatus(HttpStatus.OK)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "404", description = "Schema not found"),
+            @ApiResponse(responseCode = "404", description = "Project not found"),
             @ApiResponse(responseCode = "500", description = "Internal error")})
     public Project activateProject(@PathVariable long id) {
         return projectService.acrivateProject(id);
