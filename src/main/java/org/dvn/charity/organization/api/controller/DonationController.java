a549f5f2a12d617364215b69c4e1e99dc069f6c5
Dmitry Neganov <glorynewfound@yandex.ru>
2023-04-17 22:27:41 +0300

    feature

--- src/main/java/org/dvn/charity/organization/api/controller/DonationController.java
+++ src/main/java/org/dvn/charity/organization/api/controller/DonationController.java
@@ -7,17 +7,13 @@ import lombok.RequiredArgsConstructor;
 import org.dvn.charity.organization.api.dto.DonationDto;
 import org.dvn.charity.organization.persistence.entity.Donation;
 import org.dvn.charity.organization.service.DonationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
- * The type Donation controller.
+ * Donation controller.
  */
 @RestController
 @RequestMapping(value = "/donations")
@@ -29,9 +25,15 @@ public class DonationController {
     /**
      * Find all donations list.
      *
-     * @return the list
+     * @return the list of donations
      */
     @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get all donations list")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Donation not found"),
+            @ApiResponse(code = 500, message = "Internal error")})
     public List<Donation> findAllDonations() {
         return donationService.getAllDonations();
     }
@@ -43,6 +45,12 @@ public class DonationController {
      * @return the donation by id
      */
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get donation by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Donation not found"),
+            @ApiResponse(code = 500, message = "Internal error")})
     public Donation getDonationById(@PathVariable("id") long id) {
         return donationService.getDonationById(id);
     }
@@ -54,7 +62,8 @@ public class DonationController {
      * @return the donation
      */
     @PostMapping("/")
-    @Operation
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create new donation")
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Successfully created"),
             @ApiResponse(code = 400, message = "Missing or invalid request body"),
